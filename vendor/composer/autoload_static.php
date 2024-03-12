7fa065dcc26f1e94ec093782b03ef3e3911ab9c4
cuens <sofcue4120@gmail.com>
2024-03-12 19:58:23 -0400

    aws

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -49,8 +49,8 @@ class ComposerStaticInit0b704136ba5d55a10394c6bc9d623091
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '3ea6b57890d981cdc1a34f4bf1aee5468419490d',
+        'reference' => '1f18925fbe660f9eb61b71824c8deda2b1f58e1d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '3ea6b57890d981cdc1a34f4bf1aee5468419490d',
+            'reference' => '1f18925fbe660f9eb61b71824c8deda2b1f58e1d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
